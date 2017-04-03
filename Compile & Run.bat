c064b0190830b2e39c64ee43705001d63d28db5f
DESKTOP-HTVKR9N\Kaan <kaan135@gmail.com>
2017-04-03 16:34:10 +0300

    Button and Checkboxes are working

--- Compile & Run.bat
+++ Compile & Run.bat
@@ -1,3 +1,13 @@
+
 python -m py_compile "Password Generator.py"
 
-"Password Generator.py"
+del "Password Generator.pyw"
+ren "Password Generator.pyc" "Password Generator.pyw"
+
+@echo off
+echo.
+echo  ******************************************************
+echo  * Compile successful, you can close this window now. *
+echo  ******************************************************
+
+"Password Generator.pyw"
--- Password Generator.py
+++ Password Generator.py
@@ -1,10 +1,15 @@
-from time import sleep
 import tkFont
+import string
+import random
 from Tkinter import *
 
 
 class Frame():
     root = Tk()
+    global useUppercase, useNumbers, useSymbols
+    useUppercase = True
+    useNumbers = True
+    useSymbols = False
 
     def __init__(self):
         self.root.protocol('WM_DELETE_WINDOW', self.exitProgram)
@@ -15,7 +20,7 @@ class Frame():
         self.btn_generate = Button(self.root, text='Generate', height=5, width=10, font=tkFont.Font(size=13), command=self.generatePassword)
         self.btn_generate.grid(row=0, column=0, rowspan=2)
 
-        self.lbl_progress = Label(self.root, height=2, width=22, background='black', foreground='yellow', font=tkFont.Font(size=25),  text="Ready")
+        self.lbl_progress = Label(self.root, text='Ready', height=2, width=22, background='black', foreground='yellow', font=tkFont.Font(size=25))
         self.lbl_progress.grid(row=0, column=1)
 
         self.txt_password = Text(self.root, height=3, width=22, font=tkFont.Font(size=15))
@@ -29,26 +34,55 @@ class Frame():
 
         self.chb_uppercase = Checkbutton(self.root, text="Use Uppercase", width=15, command=self.switchUppercase)
         self.chb_uppercase.place(x=310, y=35)
+        self.chb_uppercase.select()
 
         self.chb_number = Checkbutton(self.root, text="Use Numbers  ", width=15, command=self.switchNumbers)
         self.chb_number.place(x=310, y=56)
+        self.chb_number.select()
 
         self.chb_symbol = Checkbutton(self.root, text="Use Symbols   ", width=15, command=self.switchSymbols)
         self.chb_symbol.place(x=310, y=77)
 
     def generatePassword(self):
-        self.txt_password.insert(INSERT, "Hello")
-        self.txt_password.tag_add("start", "1.0", "2.0")
-        self.txt_password.tag_config("start", foreground="#228B22", justify="center")
+        self.txt_password.delete(1.0, END)
+
+        for _ in range(int(self.spn_strength.get())):
+            chars = string.ascii_lowercase
+            if useUppercase:
+                chars = chars + string.ascii_uppercase
+            if useNumbers:
+                chars = chars + string.digits
+            if useSymbols:
+                chars = chars + string.punctuation
+
+            self.txt_password.insert(INSERT, random.choice(chars))
+            self.txt_password.tag_add("start", "1.0", END)
+            self.txt_password.tag_config("start", foreground="#228B22", justify="center")
+
+        self.root.clipboard_clear()
+        self.root.clipboard_append(self.txt_password.get("1.0", END))
+        self.lbl_progress["text"] = "Copied to clipboard"
 
     def switchUppercase(self):
-        print 1
+        global useUppercase
+        if useUppercase:
+            useUppercase = False
+        else:
+            useUppercase = True
 
     def switchNumbers(self):
-        print 1
+        global useNumbers
+        if useNumbers:
+            useNumbers = False
+        else:
+            useNumbers = True
 
     def switchSymbols(self):
-        print 1
+        global useSymbols
+        if useSymbols:
+            useSymbols = False
+        else:
+            useSymbols = True
 
     def exitProgram(self):
         self.root.destroy()
